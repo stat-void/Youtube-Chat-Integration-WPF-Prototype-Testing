02a6d900b783b032e0fbe99e901768f427b2b529
stat-void <stat.void@proton.me>
2024-01-22 16:07:07 +0200

    Update poll, or at least try without mirroring.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -23,6 +23,10 @@ namespace youtube_chat_integration_wpf_prototype_testing
     {
         //private readonly List<ChatMessage> _chatData = new();
         private readonly ObservableCollection<ChatMessage> _chatData = new();
+        private readonly ObservableCollection<PollMessage> _pollData = new();
+
+        private readonly Dictionary<string, string> _userNameMessageDict = new();
+        private readonly Dictionary<string, int> _messageCountDict = new();
 
         public MainWindow()
         {
@@ -31,6 +35,7 @@ namespace youtube_chat_integration_wpf_prototype_testing
             OptionsTab.Visibility = Visibility.Hidden;
 
             ChatMessagesList.ItemsSource = _chatData;
+            ChatPollingList.ItemsSource = _pollData;
         }
 
         public void BindToYoutube()
@@ -49,12 +54,36 @@ namespace youtube_chat_integration_wpf_prototype_testing
             {
                 YoutubeChatMessage message = messages[i];
 
+                // 1. Update chat message data
                 if (_chatData.Count >= 20)
                     _chatData.RemoveAt(0);
-
                 _chatData.Add(new ChatMessage() { Username = message.Username, Message = message.Message});
+
+
+                // 2. Update polling data
+                string messageKey = message.Message.Trim().ToLower();
+                if (!_userNameMessageDict.ContainsKey(messageKey))
+                {
+                    _messageCountDict[messageKey] = 1;
+                }
+                else
+                {
+                    _messageCountDict[_userNameMessageDict[message.Username]]--;
+                    _messageCountDict[messageKey]++;
+                }
+
+                // Finish changing the message of the user
+                _userNameMessageDict[message.Username] = messageKey;
+
                 Trace.WriteLine($"{message.Username} - \"{message.Message}\"");
             }
+
+            //Linq isn't ideal, but is quick enough for a prototype
+            var sortedDict = (from entry in _messageCountDict orderby entry.Value descending select entry).Take(10);
+            _pollData.Clear();
+
+            foreach (KeyValuePair<string, int> pair in sortedDict)
+                _pollData.Add(new() { Message = pair.Key, Count = pair.Value });
 
             //ChatMessagesList.Items.Refresh();
         }
@@ -89,5 +118,11 @@ namespace youtube_chat_integration_wpf_prototype_testing
     {
         public string? Username { get; set; }
         public string? Message { get; set; }
+    }
+
+    public class PollMessage
+    {
+        public string? Message { get; set; }
+        public int? Count { get; set; }
     }
 }
